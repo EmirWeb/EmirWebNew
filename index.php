a9495624c58fd0c63d191963f26376b2744893d4
Emir Hasanbegovic <emir@emirweb.com>
2017-11-06 14:16:10 -0500

    removing appcues

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
 	DomManager::addCSS('CSS/Home.css');
 	DomManager::addScript('Scripts/Home.js');
 	DomManager::addScript('Scripts/Taplytics.js');
-	DomManager::addScript('//fast.appcues.com/30050.js');
-	DomManager::addScript('Scripts/Appcues.js');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
